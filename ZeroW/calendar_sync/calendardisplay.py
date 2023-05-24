98b53bf0623b6eea9313feaf892aeb6755cefaf4
Manos Wagner <l>
2023-05-24 23:02:07 +0200

    Changed the way event times are displayed

--- ZeroW/calendar_sync/calendardisplay.py
+++ ZeroW/calendar_sync/calendardisplay.py
@@ -55,16 +55,16 @@ def getEvents():
 
 
 def formatEvents(events):
-    eventstr = ''
+    eventstr = '| '
     for index, event in enumerate(events):
         start = event['start'].get('dateTime', event['start'].get('date'))
         eventDate = start.split('-')  
         eventDate = f'{eventDate[2][:2]}.{eventDate[1]}.{eventDate[0][-2:]}'  
         if 'T' in start:
-            eventTime = start.split('T')[1].split('+')[0][:-3]
+            eventTime = ' | ' + start.split('T')[1].split('+')[0][:-3]
         else:
-            eventTime = 'whole day'
-        eventstr += f"""{eventDate} | {eventTime}
+            eventTime = ''
+        eventstr += f"""{eventDate}{eventTime}
     {event['summary']}"""
         if index < 2:
             eventstr += '\n'
